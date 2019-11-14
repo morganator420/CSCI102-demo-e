f7defb4637efef07285d200ae8bcaee0f615647f
Morgan Stoner <morganstoner@mines.edu>
2019-11-14 11:51:04 -0700

    created file that calculates the first 10 Fibonacci numbers

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+# fibonacci.py
+
+def fib():
+    fibs = [1, 2]
+
+    for i in range(1,9):
+        fibs.append(fibs[i] + fibs[i-1])
+    return fibs
+
+def main():
+    print('OUTPUT', fib())
+
+if __name__ == "__main__":
+    main()
